417a429f12716957a9de5f03d85543183d5360e4
robmatelyan <robmatelyan@gmail.com>
2021-04-09 02:19:13 -0400

    table completed

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+console.log(tableData);
+
+// reference table body
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(sightingsReport) {
+    console.log(sightingsReport);
+    var row = tbody.append("tr");
+    Object.entries(sightingsReport).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
