55cb896993aeeea2fa6dae2c063298471d4be566
robmatelyan <robmatelyan@gmail.com>
2021-04-10 17:31:25 -0400

    filter functioning

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -24,8 +24,8 @@ tableData.forEach(function(sightingsReport) {
         var cell = row.append("td");
         // insert value text into table
         cell.text(value);
+        });
     });
-});
 
 
 // // reference button
@@ -50,10 +50,69 @@ function runEnter() {
 
     // log input
     console.log(inputValue)
-    // filtered data based on form submission
-    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
 
-    console.log(filteredData);
+    if (inputValue.length == 0) {
+        tbody.html("");
+
+        return tableData.forEach(function(sightingsReport) {
+            console.log(sightingsReport);
+        
+            // reference tr
+            var row = tbody.append("tr");
+            // use Object.entries and forEach to iterate through key value pairs
+            Object.entries(sightingsReport).forEach(function([key, value]) {
+                // log key value pairs
+                console.log(key, value);
+                //reference td
+                var cell = row.append("td");
+                // insert value text into table
+                cell.text(value);
+                });
+            });
+        
+        
+        
+    }
+
+    else {
+           // filtered data based on form submission
+        var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+        console.log(filteredData); 
+        tbody.html("");
+
+        filteredData.forEach(function(filteredReport) {
+            var filteredRow = tbody.append("tr");
+            Object.entries(filteredReport).forEach(function([key, value]) {
+                var newCell = filteredRow.append("td");
+                newCell.text(value);
+            });
+        });
+    };
+
     
 };
 
+var resetButton = d3.select("#reset-btn");
+
+resetButton.on("click", resetTable);
+
+function resetTable() {
+    d3.event.preventDefault();
+    
+    tableData.forEach(function(sightingsReport) {
+        console.log(sightingsReport);
+    
+        // reference tr
+        var row = tbody.append("tr");
+        // use Object.entries and forEach to iterate through key value pairs
+        Object.entries(sightingsReport).forEach(function([key, value]) {
+            // log key value pairs
+            console.log(key, value);
+            //reference td
+            var cell = row.append("td");
+            // insert value text into table
+            cell.text(value);
+            });
+        });
+}
